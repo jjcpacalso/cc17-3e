23956e7343fec6a27ea8d285e5fb2f38ea07b8c9
jjcpacalso <jjcpacalso@uc-bcf.edu.ph>
2023-10-08 17:51:10 +0800

    Format the signature and greeting

--- HappyBirthdayCompose/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ HappyBirthdayCompose/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -44,11 +46,15 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier){
         Text(
             text = message,
             fontSize = 100.sp,
-            lineHeight = 116.sp
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
         )
         Text(
             text = from,
             fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
         )
     }
 }
