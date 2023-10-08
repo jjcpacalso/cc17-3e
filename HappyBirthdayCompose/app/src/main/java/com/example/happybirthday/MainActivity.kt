2b9ebbf3c579ba02147338defec253c179719c9b
jjcpacalso <jjcpacalso@uc-bcf.edu.ph>
2023-10-08 18:14:47 +0800

    Extract hardcoded strings as a resource

--- HappyBirthdayCompose/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ HappyBirthdayCompose/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,7 +34,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingImage(message = "Happy Birthday Sam!", from = "From Jerry")
+                    GreetingImage(
+                        message = stringResource(R.string.happy_birthday_text),
+                        from = stringResource(
+                            id = R.string.signature_text
+                        )
+                    )
                 }
             }
         }
@@ -88,6 +94,11 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier){
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingImage(message = "Happy Birthday Sam!", from = "From Jerry")
+        GreetingImage(
+            message = stringResource(R.string.happy_birthday_text),
+            from = stringResource(
+                id = R.string.signature_text
+            )
+        )
     }
 }
